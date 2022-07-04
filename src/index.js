5f8c1b2b75760c3ecc18fe9b18c1c13aa464caf3
Anders Wiberg Olsen <anders@wiberg.tech>
2022-07-04 18:37:38 +0200

    Refactor to be a bit more neat

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
 const nasaKey = process.env.NASA_KEY;
 
-getDailyImage();
+updateDailyImage();
+
+async function updateDailyImage() {
+    const imageElement = document.getElementById('dailyImage');
+    const imageTitleElement = document.getElementById('dailyImageTitle');
+
+    const getDailyImagePromise = getDailyImage();
+
+    const { title, url } = await getDailyImagePromise;
+
+    imageElement.src = url;
+    imageElement.alt = title;
+    imageTitleElement.textContent = title;
+}
 
 async function getDailyImage() {
     const dailyImageUrl = `https://api.nasa.gov/planetary/apod?api_key=${nasaKey}`;
 
-    const imageElement = document.getElementById('dailyImage');
-    const imageTitleElement = document.getElementById('dailyImageTitle');
-
     const response = await fetch(dailyImageUrl);
-    if (response.status != 200) {
-        imageTitleElement.textContent = 'ERROR: Failed to load image';
-        return;
-    }
-
-    console.log(response);
     const jsonResponse = await response.json();
-    console.log(jsonResponse);
+    console.log('NASA JSON Response', jsonResponse);
 
-    const imageUrl = jsonResponse.url;
-    const imageTitle = jsonResponse.title;
-    imageElement.src = imageUrl;
-    imageElement.alt = imageTitle;
-    imageTitleElement.textContent = imageTitle;
+    return {
+        title: jsonResponse.title,
+        url: jsonResponse.url
+    };
 }
